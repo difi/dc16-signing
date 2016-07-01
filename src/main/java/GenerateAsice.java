f95f7e5f3427ff42dcb606e74926cb9462a9b3d8
EirikUlversoy <Eulverso@gmail.com>
2016-07-01 10:04:22 +0200

    creates asice, dumps it as zip to disk

--- src/main/java/GenerateAsice.java
+++ src/main/java/GenerateAsice.java
@@ -1,27 +1,27 @@
-import ch.qos.logback.core.net.ssl.KeyStoreFactoryBean;
 import no.digipost.signature.api.xml.thirdparty.asice.ASiCManifest;
-import no.digipost.signature.api.xml.thirdparty.xmldsig.Signature;
 import no.digipost.signature.client.ClientConfiguration;
 import no.digipost.signature.client.asice.*;
 import no.digipost.signature.client.asice.archive.CreateZip;
 import no.digipost.signature.client.asice.manifest.CreateDirectManifest;
 import no.digipost.signature.client.asice.manifest.Manifest;
 import no.digipost.signature.client.asice.manifest.ManifestCreator;
-import no.digipost.signature.client.asice.signature.CreateSignature;
-import no.digipost.signature.client.core.Document;
+import no.digipost.signature.client.asice.signature.*;
 import no.digipost.signature.client.core.Sender;
 import no.digipost.signature.client.core.SignatureJob;
-import no.digipost.signature.client.core.exceptions.CertificateException;
 import no.digipost.signature.client.direct.*;
 import no.digipost.signature.client.security.KeyStoreConfig;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.batch.BatchProperties;
 
 import java.io.*;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
 import java.security.*;
+import java.security.Signature;
+import java.security.cert.CertificateException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
+
+import static no.motif.Strings.bytes;
 
 public class GenerateAsice {
 
@@ -31,83 +31,73 @@ public class GenerateAsice {
     private Signature signature;
     private ClientConfiguration clientConfiguration;
     private KeyStore keyStore;
+    private KeyStoreConfig keyStoreConfig;
     private ManifestCreator manifestCreator = new CreateDirectManifest();
+    List<ASiCEAttachable> files = new ArrayList<>();
+
     GenerateAsice(){};
 
-    public void createAsice() throws KeyStoreException, NoSuchAlgorithmException,NoSuchProviderException, FileNotFoundException, IOException,java.security.cert.CertificateException {
-        InputStream inputStream = new ByteArrayInputStream("example".getBytes());
+    public void setupKeystoreConfig(){
+        try {
+            this.keyStore = KeyStore.getInstance("JKS");
+            this.keyStore.load(new FileInputStream("C:\\Users\\camp-eul\\Documents\\GitHub\\dc16-signing\\src\\main\\resources\\kontaktinfo-client-test.jks"),"changeit".toCharArray());
+            this.keyStoreConfig = KeyStoreConfig.fromKeyStore(new FileInputStream("C:\\Users\\camp-eul\\Documents\\GitHub\\dc16-signing\\src\\main\\resources\\kontaktinfo-client-test.jks")
+                    ,keyStore.aliases().nextElement(),"changeit","changeit");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
-        KeyStore KS = KeyStore.getInstance("JKS");
-        KS.load(new FileInputStream("C:\\Users\\camp-eul\\Documents\\GitHub\\dc16-signing\\src\\main\\resources\\kontaktinfo-client-test.jks"),"changeit".toCharArray());
-        System.out.println(KS.toString());
+    public DirectDocument pdfToDocument(String pdfPath) {
 
-        String nextAlias = KS.aliases().nextElement();
-        System.out.println(nextAlias);
-        //for (KS.aliases().nextElement(); KS.aliases().hasMoreElements();)
-        //    System.out.println(KS.aliases().nextElement());
+        byte[] pdfBytes = new byte[(int)pdfPath.length()];
+        try {
+            new DataInputStream(new FileInputStream(pdfPath)).readFully(pdfBytes);
+            DirectDocument document = DirectDocument.builder("Subject", "Dokument til signering.pdf", pdfBytes).build();
+            return document;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+    public DocumentBundle createAsice() throws KeyStoreException, NoSuchAlgorithmException,NoSuchProviderException, FileNotFoundException, IOException,java.security.cert.CertificateException {
 
 
-
-
-        List<ASiCEAttachable> files = new ArrayList<>();
-        CreateSignature signatureC = new CreateSignature();
-        Manifest manifest = new Manifest("Example".getBytes());
-
-        byte[] documentBytes = "Example".getBytes();
-        this.clientConfiguration = ClientConfiguration.builder(KeyStoreConfig.fromKeyStore(new FileInputStream("C:\\Users\\camp-eul\\Documents\\GitHub\\dc16-signing\\src\\main\\resources\\kontaktinfo-client-test.jks")
-                ,nextAlias,"changeit","changeit"))
+        this.clientConfiguration = ClientConfiguration.builder(keyStoreConfig)
                 .globalSender(new Sender("123456789"))
                 .build();
-        System.out.println(clientConfiguration.toString());
 
         this.createASiCE = new CreateASiCE(manifestCreator,clientConfiguration);
-        System.out.println(this.createASiCE.toString());
+
         DirectSigner signer = DirectSigner.builder("12345678910").build();
-
-        DirectDocument document = DirectDocument.builder("Subject", "Dokument til signering.pdf", documentBytes).build();
-
-        DirectClient directClient = new DirectClient(clientConfiguration);
-
-
+        DirectDocument document = pdfToDocument("C:\\Users\\camp-eul\\Documents\\GitHub\\dc16-signing\\src\\main\\resources\\Dokument til signering.pdf");
         SignatureJob signatureJob = new DirectJob.Builder(signer,document,"http://sender.org/onCompletion","http://sender.org/onRejection","http://sender.org/onError").build();
 
-        DirectJobResponse directJobResponse = directClient.create((DirectJob)signatureJob);
-        System.out.println(directJobResponse.getStatusUrl());
+        DocumentBundle asice = createASiCE.createASiCE(signatureJob);
+        dumper(asice,signatureJob);
+        
 
+        return asice;
+    }
 
-        files.add(manifest);
-
-
-        System.out.println(document.getFileName().toString());
-
-
-        ExitUrls exitUrls = ExitUrls.of(
-                "http://sender.org/onCompletion",
-                "http://sender.org/onRejection",
-                "http://sender.org/onError"
-        );
-        ASiCManifest asicManifest = new ASiCManifest();
-
-
-
-
-        DirectJob directJob = DirectJob.builder(DirectSigner.builder("12345678910").build(), document, exitUrls).build();
-        File archiveOutputFile = new File(System.getProperty("java.io.tmpdir"), "asic-sample-default.zip");
-        CreateZip createZip = new CreateZip();
-        documentBytes = createZip.zipIt(files);
-
-
-        CreateSignature createSignature = new CreateSignature();
-
-        createSignature.createSignature(files,KeyStoreConfig.fromKeyStore(inputStream,"","",""));
-
-
-
+    /**
+     * Dumps a zip file of the document bundle to disk.
+     * @param documentBundle
+     * @param signatureJob
+     * @throws IOException
+     */
+    public void dumper(DocumentBundle documentBundle, SignatureJob signatureJob) throws IOException {
+        Path path = FileSystems.getDefault().getPath("","");
+        DumpDocumentBundleToDisk dumper = new DumpDocumentBundleToDisk(path);
+        InputStream asiceInputStream = documentBundle.getInputStream();
+        dumper.process(signatureJob,asiceInputStream);
 
     }
 
     public static void main(String[] args) throws KeyStoreException, java.security.cert.CertificateException, NoSuchAlgorithmException, NoSuchProviderException, IOException {
         GenerateAsice generateAsice = new GenerateAsice();
+        generateAsice.setupKeystoreConfig();
+
         generateAsice.createAsice();
 
     }
