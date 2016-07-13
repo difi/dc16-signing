fa03737051695ef59889e8653d0f38aeb64bea9a
NoraTomas <noratomas@hotmail.com>
2016-07-13 12:23:26 +0200

    fixed corrupt signed pAdES

--- signing-digipost/src/main/java/SignedDocumentFetcher.java
+++ signing-digipost/src/main/java/SignedDocumentFetcher.java
@@ -30,14 +30,22 @@ public class SignedDocumentFetcher {
     public String getPades() throws IOException{
 
     if (this.statusReader.getStatusResponse().is(this.statusReader.getStatusResponse().getStatus().SIGNED)) {
-        InputStream pAdESStream = client.getPAdES(this.statusReader.getStatusResponse().getpAdESUrl());
-        byte[] buffer = new byte[pAdESStream.available()];
-        pAdESStream.read(buffer);
 
-        File targetFile = new File(System.getProperty("user.dir") + "targetFile2.pdf");
-        OutputStream outStream = new FileOutputStream(targetFile);
-        outStream.write(buffer);
-        this.statusReader.confirmProcessedSignatureJob();
+        InputStream pAdESStream = client.getPAdES(this.statusReader.getStatusResponse().getpAdESUrl());
+
+        OutputStream outputStream = new FileOutputStream(System.getProperty("user.dir") + "pAdESTest.pdf");
+        int read = 0;
+
+        byte[] bytes = new byte[999999999];
+        while ((read = pAdESStream.read(bytes)) != -1) {
+            outputStream.write(bytes, 0, read);
+        }
+        //pAdESStream.read(buffer);
+
+        //File targetFile = new File(System.getProperty("user.dir") + "pAdES.pdf");
+        //OutputStream outStream = new FileOutputStream(targetFile);
+        //outStream.write(buffer);
+        //this.statusReader.confirmProcessedSignatureJob();
 
         return "fetched pade";
     }
@@ -47,11 +55,11 @@ public class SignedDocumentFetcher {
     public String getXades() throws IOException{
 
         if (this.statusReader.getStatusResponse().is(this.statusReader.getStatusResponse().getStatus().SIGNED)) {
-            InputStream xAdESStream = client.getPAdES(this.statusReader.getStatusResponse().getpAdESUrl());
+            InputStream xAdESStream = client.getXAdES(this.statusReader.getStatusResponse().getxAdESUrl());
             byte[] buffer = new byte[xAdESStream.available()];
             xAdESStream.read(buffer);
 
-            File targetFile = new File(System.getProperty("user.dir") + "targetFile2.pdf");
+            File targetFile = new File(System.getProperty("user.dir") + "xAdES.xml");
             OutputStream outStream = new FileOutputStream(targetFile);
             outStream.write(buffer);
             this.statusReader.confirmProcessedSignatureJob();
