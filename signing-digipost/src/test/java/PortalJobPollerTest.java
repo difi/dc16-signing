0ee142592a8a102f552de7c2ef6d3a301cf9d653
EirikUlversoy <Eulverso@gmail.com>
2016-07-13 12:43:14 +0200

    attempt at portaljobpollertest

--- signing-digipost/src/test/java/PortalJobPollerTest.java
+++ signing-digipost/src/test/java/PortalJobPollerTest.java
@@ -1,4 +1,45 @@
+import no.digipost.signature.client.portal.PortalClient;
+import no.digipost.signature.client.portal.PortalJobStatus;
+import no.digipost.signature.client.portal.PortalJobStatusChanged;
+import org.mockito.Mock;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 public class PortalJobPollerTest {
+    @Mock
+    private static PortalClient client;
+
+    @Mock
+    PortalJobStatusChanged statusChange;
+
+    public static void setUp(){
+        client = mock(PortalClient.class);
+        PortalJobStatusChanged mockedStatusChanged = mock(PortalJobStatusChanged.class);
+        when(client.getStatusChange().getStatus()).thenReturn(PortalJobStatus.IN_PROGRESS);
+        when(mockedStatusChanged.getStatus().toString()).thenReturn("IN_PROGRESS");
+
+    }
+
+    public void testHasPolled(){
+
+    }
+
+    public void testHasNotPolled(){
+        PortalJobPoller poller = new PortalJobPoller(mockedClient);
+    }
+
+    public void testPadesReady(){
+
+    }
+
+    public void testXadesReady(){
+
+    }
+
+    public void testPollStatus(){
+
+    }
 
 
 }
