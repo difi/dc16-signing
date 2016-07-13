99e7493874780c71bcf9b85d7cac711257e81609
NoraTomas <noratomas@hotmail.com>
2016-07-13 12:28:51 +0200

    fixed corrupt signed pAdES

--- signing-digipost/src/main/java/SignedDocumentFetcher.java
+++ signing-digipost/src/main/java/SignedDocumentFetcher.java
@@ -29,27 +29,27 @@ public class SignedDocumentFetcher {
 
     public String getPades() throws IOException{
 
-    if (this.statusReader.getStatusResponse().is(this.statusReader.getStatusResponse().getStatus().SIGNED)) {
+        if (this.statusReader.getStatusResponse().is(this.statusReader.getStatusResponse().getStatus().SIGNED)) {
 
-        InputStream pAdESStream = client.getPAdES(this.statusReader.getStatusResponse().getpAdESUrl());
+            InputStream pAdESStream = client.getPAdES(this.statusReader.getStatusResponse().getpAdESUrl());
 
-        OutputStream outputStream = new FileOutputStream(System.getProperty("user.dir") + "pAdESTest.pdf");
-        int read = 0;
+            OutputStream outputStream = new FileOutputStream(System.getProperty("user.dir") + "pAdESTest.pdf");
+            int read = 0;
 
-        byte[] bytes = new byte[999999999];
-        while ((read = pAdESStream.read(bytes)) != -1) {
-            outputStream.write(bytes, 0, read);
+            byte[] bytes = new byte[999999999];
+            while ((read = pAdESStream.read(bytes)) != -1) {
+                outputStream.write(bytes, 0, read);
+            }
+            //pAdESStream.read(buffer);
+
+            //File targetFile = new File(System.getProperty("user.dir") + "pAdES.pdf");
+            //OutputStream outStream = new FileOutputStream(targetFile);
+            //outStream.write(buffer);
+            //this.statusReader.confirmProcessedSignatureJob();
+
+            return "fetched pade";
         }
-        //pAdESStream.read(buffer);
-
-        //File targetFile = new File(System.getProperty("user.dir") + "pAdES.pdf");
-        //OutputStream outStream = new FileOutputStream(targetFile);
-        //outStream.write(buffer);
-        //this.statusReader.confirmProcessedSignatureJob();
-
-        return "fetched pade";
-    }
-    else return "failed";
+        else return "failed";
     }
 
     public String getXades() throws IOException{
