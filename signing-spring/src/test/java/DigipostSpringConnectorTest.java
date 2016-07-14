bdf08ba274b28e56339695a64026732aefe803f4
NoraTomas <noratomas@hotmail.com>
2016-07-14 08:54:10 +0200

    working test framework

---
+++ signing-spring/src/test/java/DigipostSpringConnectorTest.java
@@ -0,0 +1,12 @@
+import org.testng.annotations.Test;
+
+/**
+ * Created by camp-nto on 14.07.2016.
+ */
+public class DigipostSpringConnectorTest {
+
+    @Test
+    public void sample(){
+        
+    }
+}
