b082b5f31043a561742dd062803a4ba2c101e576
EirikUlversoy <Eulverso@gmail.com>
2016-07-21 14:25:35 +0200

    more readable pades/xades methods

--- signing-digipost/src/main/java/SignedDocumentFetcher.java
+++ signing-digipost/src/main/java/SignedDocumentFetcher.java
@@ -38,31 +38,12 @@ public class SignedDocumentFetcher {
 
     public byte[] getPades() throws IOException {
         PAdESReference pAdESReference = null;
-        try{
         DirectJobStatusResponse directJobStatusResponse = this.statusReader.getStatusResponse();
-
         if (directJobStatusResponse.is(directJobStatusResponse.getStatus().SIGNED)) {
             pAdESReference = directJobStatusResponse.getpAdESUrl();
-            XAdESReference xAdESReference = directJobStatusResponse.getxAdESUrl();
 
-            InputStream pAdESStream = client.getPAdES(pAdESReference);
-            System.out.println(pAdESReference.getpAdESUrl());
-            System.out.println(xAdESReference.getxAdESUrl());
-            OutputStream outputStream = new FileOutputStream(System.getProperty("user.dir") + "pAdESTest.pdf");
-            int read = 0;
-
-            byte[] bytes = new byte[999999999];
-            while ((read = pAdESStream.read(bytes)) != -1) {
-                outputStream.write(bytes, 0, read);
-            }
-        }
-        } catch (TooEagerPollingException eagerPollingException) {
-                String nextAvailablePollingTime = eagerPollingException.getNextPermittedPollTime().toString();
-                System.out.print(nextAvailablePollingTime);
-                //return "Too frequent polling, please wait until " + nextAvailablePollingTime;
-                return "".getBytes();
-            }
             return ByteStreams.toByteArray(client.getPAdES(pAdESReference));
+        } else return "".getBytes();
     }
 
 
@@ -72,14 +53,6 @@ public class SignedDocumentFetcher {
         DirectJobStatusResponse directJobStatusResponse = this.statusReader.getStatusResponse();
         if (directJobStatusResponse.is(directJobStatusResponse.getStatus().SIGNED)) {
             xAdESReference = directJobStatusResponse.getxAdESUrl();
-            InputStream xAdESStream = client.getXAdES(this.statusReader.getStatusResponse().getxAdESUrl());
-            byte[] buffer = new byte[xAdESStream.available()];
-            xAdESStream.read(buffer);
-
-            File targetFile = new File(System.getProperty("user.dir") + "xAdES.xml");
-            OutputStream outStream = new FileOutputStream(targetFile);
-            outStream.write(buffer);
-            this.statusReader.confirmProcessedSignatureJob();
 
             return ByteStreams.toByteArray(client.getXAdES(xAdESReference));
         } else return "".getBytes();
