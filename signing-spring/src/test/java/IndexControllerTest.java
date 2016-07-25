8472d27f87baeabd910c957d71a77afbd1ca3300
NoraTomas <noratomas@hotmail.com>
2016-07-25 08:40:14 +0200

    added tests

--- signing-spring/src/test/java/IndexControllerTest.java
+++ signing-spring/src/test/java/IndexControllerTest.java
@@ -1,15 +1,28 @@
 import controllers.IndexController;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /**
  * Created by camp-nto on 21.07.2016.
  */
 public class IndexControllerTest {
 
+    private MockMvc mvc = MockMvcBuilders.standaloneSetup(new IndexController()).build();;
+
     @Test
     public void getHomePage_returns_index(){
         IndexController indexController = new IndexController();
         Assert.assertEquals(indexController.getHomePage(), "index");
+    }
+
+    @Test
+    public void gotLogin_returns_simulertLogin() throws Exception {
+       IndexController indexController = new IndexController();
+        Assert.assertEquals(indexController.goToLogin(), "simulertLogin");
     }
 }
