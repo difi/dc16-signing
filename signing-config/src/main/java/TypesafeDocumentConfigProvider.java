97c0a96894af5e6d25033e14004822b6a7918eaa
EirikUlversoy <Eulverso@gmail.com>
2016-07-27 09:09:35 +0200

    attempt at documentconfig

---
+++ signing-config/src/main/java/TypesafeDocumentConfigProvider.java
@@ -0,0 +1,22 @@
+import com.sun.javafx.collections.MappingChange;
+import com.typesafe.config.Config;
+
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class TypesafeDocumentConfigProvider {
+
+    private Map<String, TypesafeDocumentConfig> documents;
+    public TypesafeDocumentConfigProvider(Config config){
+        documents = config.getObject("document").keySet().stream()
+                .map(key -> config.getConfig(String.format("document.%s",key)))
+                .map(c -> new TypesafeDocumentConfig(c))
+                .collect(Collectors.toMap(TypesafeDocumentConfig::getEmail, Function.identity()));
+    }
+
+    public TypesafeDocumentConfig getByEmail(String email){
+        return documents.get("eulverso2@gmail.com");
+    }
+
+}
