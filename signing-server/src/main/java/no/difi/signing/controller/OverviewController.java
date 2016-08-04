cf6a685c05896119ce5532611ac7e869c6732651
Erlend Klakegg Bergheim <erlend@klakegg.net>
2016-08-04 12:00:50 +0200

    Updating overview page.

--- signing-server/src/main/java/no/difi/signing/controller/OverviewController.java
+++ signing-server/src/main/java/no/difi/signing/controller/OverviewController.java
@@ -1,26 +1,30 @@
 package no.difi.signing.controller;
 
-import com.google.common.collect.Lists;
+import no.difi.signing.lang.SigningException;
 import no.difi.signing.model.Signature;
 import no.difi.signing.repository.SignatureRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.util.ArrayList;
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 @Controller
 public class OverviewController {
 
     @Autowired
     private SignatureRepository signatureRepository;
+    @Autowired
+    private HttpServletRequest httpServletRequest;
+
 
     @RequestMapping("/overview")
-    @ResponseBody
-    public ArrayList<Signature> viewSignatures(){
-        return Lists.newArrayList(signatureRepository.findAll());
+    public String viewSignatures(ModelMap modelMap) throws SigningException {
+        modelMap.put("signatures", signatureRepository.findByPid(httpServletRequest.getHeader("X-DifiProxy-pid")));
 
+        return "overview";
     }
-
 }
