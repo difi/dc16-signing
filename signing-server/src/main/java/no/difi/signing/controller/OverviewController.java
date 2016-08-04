9d5bdbf345c0fcb69adf794e6b766092f8bd8870
Madelelo <madelelo@stud.ntnu.no>
2016-08-04 08:56:26 +0200

    Prints all signatures

--- signing-server/src/main/java/no/difi/signing/controller/OverviewController.java
+++ signing-server/src/main/java/no/difi/signing/controller/OverviewController.java
@@ -1,10 +1,14 @@
 package no.difi.signing.controller;
 
+import com.google.common.collect.Lists;
+import no.difi.signing.model.Signature;
 import no.difi.signing.repository.SignatureRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
 
 @Controller
 public class OverviewController {
@@ -14,10 +18,8 @@ public class OverviewController {
 
     @RequestMapping("/overview")
     @ResponseBody
-    public String viewSignatures(){
-
-       String signatures =  signatureRepository.findAll().iterator().toString();
-        return signatures;
+    public ArrayList<Signature> viewSignatures(){
+        return Lists.newArrayList(signatureRepository.findAll());
 
     }
 
---
+++ signing-server/src/test/java/no/difi/signing/controller/OverviewControllerTest.java
@@ -0,0 +1,35 @@
+package no.difi.signing.controller;
+
+import no.difi.signing.TestApplication;
+import no.difi.signing.model.Signature;
+import no.difi.signing.repository.SignatureRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+@SpringBootTest(classes = TestApplication.class)
+public class OverviewControllerTest  extends AbstractTestNGSpringContextTests {
+
+    @Autowired
+    SignatureRepository signatureRepository;
+
+    @Test
+    public void printAllSignatures(){
+        Signature signature = Signature.newInstance();
+        signature.setId(0);
+        signature.setPid("12345678901234");
+        signature.setDocumentTitle("Document 1");
+        signature.setDocumentToken("document1");
+        signature.setDocumentVersion("1");
+        signature.setTimestamp(new Date());
+        signatureRepository.save(signature);
+
+        OverviewController overviewController = new OverviewController();
+        ArrayList<Signature> s = overviewController.viewSignatures();
+//        System.out.println(overviewController.viewSignatures());
+    }
+}
